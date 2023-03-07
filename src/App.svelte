d7e7d9a9f25d11f8b9cf7a75cc2f9641d05d63c2
ibro <sameoldlab@proton.me>
2023-03-07 21:57:52 +0100

    nice icons. hell yes this deserves a commit. added the files... T_T

--- src/App.svelte
+++ src/App.svelte
@@ -4,33 +4,30 @@
   import NavItem from "./lib/NavItem.svelte"
   import MainNav from "./views/MainNav.svelte"
   import TopStats from "./views/TopStats.svelte"
-  
+
   let view = ""
 </script>
 
-<main>
-  <nav>
-    <div>
-      <button on:click={() => (view = "accounts")}
-        ><NavItem name="accounts" /></button
-      >
-    </div>
-    <div>
-      <button on:click={() => (view = "log")}> <NavItem name="log" /></button>
-    </div>
-    <div>
-      <button on:click={() => (view = "blocks")}>
-        <NavItem name="blocks" />
-      </button>
-    </div>
-  </nav>
+<nav>
+  <button on:click={() => (view = "accounts")}>
+    <NavItem name="accounts" />
+  </button>
 
+  <button on:click={() => (view = "log")}>
+    <NavItem name="log" />
+  </button>
+
+  <button on:click={() => (view = "blocks")}>
+    <NavItem name="blocks" />
+  </button>
+</nav>
+<main>
   {#if view == "log"}
-  <Log />
+    <Log />
   {:else}
     <TopStats />
     {#if view == "accounts"}
-        <Accounts />
+      <Accounts />
     {/if}
   {/if}
 </main>
@@ -51,7 +48,7 @@
   nav {
     height: 100%;
     position: fixed;
-    padding: 30px 5px;
+    padding: 30px 0;
     top: 0;
     left: 0;
     background-color: black;
@@ -60,13 +57,13 @@
 
     display: flex;
     flex-direction: column;
-    gap: 2.5em;
 
     button {
       background: transparent;
       cursor: pointer;
       border: 0;
       opacity: 0.7;
+      padding: 20px 12px;
 
       &:hover {
         opacity: 1;
@@ -78,6 +75,6 @@
     }
   }
   main {
-    padding-left: 110px;
+    margin-left: 69px;
   }
 </style>
